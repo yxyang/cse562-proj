f519f7f198baf6b2fec9625670520c6674ccf686
Mohak Bhardwaj <mohak.bhardwaj@gmail.com>
2021-06-05 20:50:15 -0700

    add scipy fft

---
+++ speaker/scipy_fft.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.fftpack import fft
+from scipy.io import wavfile # get the api
+fs, data = wavfile.read('mono_22k.wav') # load the data
+channel_1 = data.T# [0] # this is a two channel soundtrack, I get the first track
+channel_1=[(ele/2**8.)*2-1 for ele in channel_1] # this is 8-bit track, b is now normalized on [-1,1)
+fft_1 = fft(channel_1) # calculate fourier transform (complex numbers list)
+
+channel_2 = data.T #[1] # this is a two channel soundtrack, I get the first track
+channel_2=[(ele/2**8.)*2-1 for ele in channel_2] # this is 8-bit track, b is now normalized on [-1,1)
+fft_2 = fft(channel_2) # calculate fourier transform (complex numbers list)
+
+d1 = len(fft_1) // 2  # you only need half of the fft list (real signal symmetry)
+k1 = np.arange(len(data))
+T1 = len(data)/fs  # where fs is the sampling frequency
+frqLabel1 = k1/T1 
+
+d2 = len(fft_1) // 2  # you only need half of the fft list (real signal symmetry)
+k2 = np.arange(len(data))
+T2 = len(data)/fs  # where fs is the sampling frequency
+frqLabel2 = k2/T2 
+
+fig, ax = plt.subplots(2,1)
+ax[0].plot(frqLabel1[:d1-1], abs(fft_1[:d1-1]),'r',)
+ax[1].plot(frqLabel2[:d2-1], abs(fft_2[:d2-1]),'r',)
+ax[0].set_title('Channel 1 FFT')
+ax[1].set_title('Channel 2 FFT') 
+
+plt.show()
+
--- speaker/speaker_stereo.py
+++ speaker/speaker_stereo.py
@@ -7,48 +7,57 @@ import sounddevice as sd
 
 
 RATE=44100
-total_time_secs = 200
+total_time_secs = 100 #200
 sweep_time_secs = 0.04
 sampling_freq = 44100
 freq_min_left = 17000
 freq_min_right = 22500
 bandwidth = 2500
 
-
-
-# ## GENERATE MONO FILE ##
-# wv = wave.open('mono.wav', 'w')
-# wv.setparams((1, 2, RATE, 0, 'NONE', 'not compressed'))
-# maxVol=2**15-1.0 #maximum amplitude
-# wvData=""
-# for i in range(0, RATE*3):
-#     wvData+=pack('h', maxVol*sin(i*500.0/RATE)) #500Hz
-# wv.writeframes(wvData)
-# wv.close()
-
-## GENERATE STEREO FILE ##
+#Generate chirps
 time = np.linspace(0, total_time_secs, total_time_secs * sampling_freq)
 time_in_period = np.fmod(time, sweep_time_secs)
 
 wave_left = 10000 * np.sin(2 * np.pi * freq_min_left * time_in_period +
                       np.pi * bandwidth * time_in_period**2 / sweep_time_secs)
 
-wave_right = 10000 * np.sin(2 * np.pi * freq_min_left * time_in_period +
+wave_right = 10000 * np.sin(2 * np.pi * freq_min_right * time_in_period +
                       np.pi * bandwidth * time_in_period**2 / sweep_time_secs)
 
 
-
-wv = wave.open('stereo.wav', 'w')
-wv.setparams((2, 2, RATE, 0, 'NONE', 'not compressed'))
+## GENERATE MONO FILES ##
+wv = wave.open('mono_17k.wav', 'w')
+wv.setparams((1, 2, RATE, 0, 'NONE', 'not compressed'))
 maxVol=2**15-1.0 #maximum amplitude
 wvData=""
+for i in range(wave_left.shape[0]):
+    wvData+=pack('h',wave_left[i])
+wv.writeframes(wvData)
+wv.close()
 
-#uncomment this part for testing. (use headphones)
+
+wv = wave.open('mono_22k.wav', 'w')
+wv.setparams((1, 2, RATE, 0, 'NONE', 'not compressed'))
+# maxVol=2**15-1.0 #maximum amplitude
+wvData=""
+for i in range(wave_right.shape[0]):
+    wvData+=pack('h',wave_right[i])
+wv.writeframes(wvData)
+wv.close()
+
+
+##GENERATE STEREO FILE
+wv = wave.open('stereo.wav', 'w')
+wv.setparams((2, 2, RATE, 0, 'NONE', 'not compressed'))
+# maxVol=2**15-1.0 #maximum amplitude
+wvData=""
+
+#uncomment this loop for testing. (use headphones)
 # for i in range(0, RATE*10):
     # wvData+=pack('h', maxVol*sin(i*500.0/RATE)) #500Hz left
-    # wvData+=pack('h', 2**10*sin(i*500.0/RATE)) #500Hz left
+    # wvData+=pack('h', 2**10*sin(i*500.0/RATE)) #500Hz right (lower volume)
     #####################
-    # wvData+=pack('h', maxVol*sin(i*200.0/RATE)) #200Hz right
+    # #### wvData+=pack('h', maxVol*sin(i*200.0/RATE)) #200Hz right
 
 for i in range(wave_left.shape[0]):
 	wvData+=pack('h', wave_left[i])
